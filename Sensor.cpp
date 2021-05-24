81ba3ddb1733ce814b71cd5bcf3e10df05b2c43e
Jonathan Sharp <jonathan.sharp@xcis.com.au>
2021-05-24 23:40:06 +0900

    V2.0 Version

---
+++ Sensor.cpp
@@ -0,0 +1,28 @@
+#include "Arduino.h"
+#include "Sensor.h"
+Sensor::Sensor()
+{
+    #ifdef debug
+    Serial.println("Sensor::Constructor");
+    #endif
+    initialised = "false";
+    lastScanTime = 0;
+}
+void Sensor::displaySensor()
+{
+    #ifdef debug
+    Serial.print(loraID + ",");
+    Serial.print(deviceType + ",");
+    Serial.print(deviceMode + ",");
+    Serial.print(String(lastScanTime) + ",");
+    Serial.print(initialised + ",");
+    Serial.println(sensorData);
+    #endif
+}
+String Sensor::outputSensor()
+{
+    String output;
+    output = loraID + "," + deviceType + "," + deviceMode + "," +
+    String(lastScanTime) + "," + initialised + "," + sensorData;
+    return output;
+}
---
+++ XcisSensor.cpp
@@ -0,0 +1,163 @@
+#include "Arduino.h"
+#include "XcisSensor.h"
+XcisSensor::XcisSensor()
+{
+  // Constructor
+  #ifdef debug
+  Serial.println("XcisSensor::Constructor");
+  #endif
+  nextSensor = 0;
+}
+int XcisSensor::getSensorListLength()
+{
+    int sensorCount = 0;
+    for (int i =0; i < numberOfSensors; i++)
+    {
+        if (sensors[i].initialised == "false")
+        {
+            return sensorCount;
+        }
+        sensorCount++;
+    }
+    return 0;
+}
+int XcisSensor::scanNextSensor()
+{
+    //Serial.println("XcisSensor::scanNextSensor");
+    if (nextSensor == numberOfSensors)
+        nextSensor = 0;
+    int sensorToScan = nextSensor;
+    if (sensors[sensorToScan].initialised == "false")
+    {
+        nextSensor = 0;
+        return -1;
+    }
+    nextSensor++;
+    return sensorToScan;
+}
+String XcisSensor::getSensorLoraID(int scanNumber)
+{
+    return sensors[scanNumber].loraID;
+}
+String XcisSensor::getSensorData(int scanNumber)
+{
+    return sensors[scanNumber].sensorData;
+}
+String XcisSensor::getSensorData(String loraID)
+{
+    String sensorData = "NOT FOUND";
+    for (int i =0; i< numberOfSensors; i++)
+    {
+        if (sensors[i].loraID.toInt() == loraID.toInt())
+        {
+            #ifdef debug
+            Serial.print("Found sensor Data:" + String(i)+ ":");
+            sensors[i].displaySensor();
+            #endif
+            //sensorData = sensors[i].loraID + ":" + sensors[i].sensorData;
+            sensorData = sensors[i].sensorData;
+            break;
+        }
+    }
+    return sensorData;
+}
+int XcisSensor::getSensorScanNumber(String loraID)
+{
+    int counter = 0;
+    for (int i =0; i< numberOfSensors; i++)
+    {
+        counter = i;
+        if (sensors[i].loraID.toInt() == loraID.toInt())
+        {
+            #ifdef debug
+            Serial.print("Found sensor:" + String(i)+ ":");
+            #endif
+            sensors[i].displaySensor();
+            break;
+        }
+        
+    }
+    return counter;
+}
+String XcisSensor::listSensors(bool serial)
+{
+    //Serial.println("XcisSensor::listSensors");
+    String output;
+    for (int i =0; i< numberOfSensors; i++)
+    {
+        if (serial == false)
+        {
+            Serial.print("ScanNumber:");
+            Serial.print(i);
+            Serial.print(",");
+            sensors[i].displaySensor();
+        }
+        else
+        {
+            output += "Sensor,";
+            output += String(i);
+            output +=  ",";
+            output += sensors[i].outputSensor();
+            output +="\n";
+        }
+        
+    }
+    return output;
+}
+void XcisSensor::checkSensorsOnline()
+{
+    unsigned long timeCheck = 0;
+    //Serial.println("XcisSensor::checkSensorsOnline");
+    for (int i =0; i< numberOfSensors; i++)
+    {
+        sensors[i].displaySensor();
+        timeCheck = millis() - 400000;
+        if (timeCheck < 0 )
+            return;
+        if (sensors[i].lastScanTime < timeCheck)
+        {
+            //Serial.println("Sensor offline");
+            setDeviceMode(i, "OFFLINE", millis());
+            sensors[i].displaySensor();
+
+        }
+    }
+}
+void XcisSensor::displaySensor(int scanNumber)
+{
+    sensors[scanNumber].displaySensor();
+}
+void XcisSensor::streamSensor(int scanNumber)
+{
+    sensors[scanNumber].outputSensor();
+}
+void XcisSensor::addSensor(int scanNumber, String loraID, String deviceType)
+{
+    //Serial.println("XcisSensor::addSensor");
+    sensors[scanNumber].setLoraID(loraID);
+    sensors[scanNumber].setdeviceType(deviceType);
+    sensors[scanNumber].setdeviceMode("UNKNOWN");
+    sensors[scanNumber].setsensorData("NODATA,");
+    sensors[scanNumber].setInit("true");
+}
+void XcisSensor::deleteSensor()
+{
+    //Serial.println("XcisSensor::deleteSensor");
+
+}
+void XcisSensor::setSensorData(int scanNumber, String data)
+{
+    //Serial.println("XcisSensor::setSensorData");
+    sensors[scanNumber].setsensorData(data);
+}
+void XcisSensor::setDeviceMode(int scanNumber, String state, unsigned long timeStamp)
+{
+    //Serial.println("XcisSensor::setdeviceMode");
+    sensors[scanNumber].setdeviceMode(state);
+    sensors[scanNumber].setLastScanTime(timeStamp);
+}
+String XcisSensor::getDeviceMode(String loraID)
+{
+    //Serial.println("XcisSensor::getDeviceMode");
+    return sensors[getSensorScanNumber(loraID)].deviceMode;
+}
